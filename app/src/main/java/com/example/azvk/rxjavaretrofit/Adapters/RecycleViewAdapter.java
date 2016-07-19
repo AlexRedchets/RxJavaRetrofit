6255f78a9702180b68e49decce05852d26c33972
AlexRedchets <alexey.redchets@gmail.com>
2016-07-19 13:09:10 -0400

    successfully get playersInfo

---
+++ app/src/main/java/com/example/azvk/rxjavaretrofit/Adapters/RecycleViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.azvk.rxjavaretrofit.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.azvk.rxjavaretrofit.Models.Player;
+import com.example.azvk.rxjavaretrofit.R;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder>{
+
+    private List<Player> listPlayer;
+    private Context context;
+    private String TAG = RecycleViewAdapter.class.getSimpleName();
+
+    public RecycleViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void updateAdapter(List<Player> lists){
+        listPlayer = lists;
+        notifyDataSetChanged();
+        Log.i(TAG, "Adapter is updated");
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPlayer != null ? listPlayer.size() : 0;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View view = inflater.inflate(R.layout.custom_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Player currentPlayerData = listPlayer.get(position);
+
+        holder.playerName.setText(currentPlayerData.getName());
+        Picasso.with(context).load(currentPlayerData.getImgRes()).into(holder.playerImage);
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView playerName;
+        ImageView playerImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            playerImage = (ImageView)itemView.findViewById(R.id.playerImage);
+            playerName = (TextView)itemView.findViewById(R.id.playerName);
+        }
+    }
+
+}
--- app/src/main/java/com/example/azvk/rxjavaretrofit/fragments/ButtonsFragment.java
+++ app/src/main/java/com/example/azvk/rxjavaretrofit/fragments/ButtonsFragment.java
@@ -1,9 +1,10 @@
 package com.example.azvk.rxjavaretrofit.fragments;
 
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.azvk.rxjavaretrofit.Adapters.RecycleViewAdapter;
 import com.example.azvk.rxjavaretrofit.Clients.PlayerClient;
 import com.example.azvk.rxjavaretrofit.Generator;
 import com.example.azvk.rxjavaretrofit.Models.Player;
@@ -19,13 +21,14 @@ import com.example.azvk.rxjavaretrofit.R;
 import java.util.List;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class ButtonsFragment extends Fragment {
 
     private static final String TAG = ButtonsFragment.class.getSimpleName();
+    private RecycleViewAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
@@ -36,16 +39,20 @@ public class ButtonsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupView(view);
 
-        Button get_button = (Button)getView().findViewById(R.id.get_button);
+        /*Button get_button = (Button)getView().findViewById(R.id.get_button);
         Button clear_button = (Button)getView().findViewById(R.id.clear_button);
+
+
 
         get_button.setOnClickListener(this::onGetButtonClicked);
 
-        clear_button.setOnClickListener(this::onClearButtonClicked);
+        clear_button.setOnClickListener(this::onClearButtonClicked);*/
     }
 
     private void onGetButtonClicked(View view) {
+
         Toast.makeText(getActivity(), "GetButtonClicked", Toast.LENGTH_SHORT).show();
 
         PlayerClient client = Generator.createService(PlayerClient.class);
@@ -54,7 +61,7 @@ public class ButtonsFragment extends Fragment {
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(playerData -> {
-                    Log.i(TAG, playerData.get(0).getName());
+                    adapter.updateAdapter(playerData);
                 },
                         throwable -> Log.e("Error", throwable.getMessage()));
 
@@ -63,5 +70,14 @@ public class ButtonsFragment extends Fragment {
 
     private void onClearButtonClicked(View view) {
         Toast.makeText(getActivity(), "ClearButtonClicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupView(View view) {
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycleView);
+        //recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecycleViewAdapter(getActivity()) ;
+        recyclerView.setAdapter(adapter);
     }
 }
